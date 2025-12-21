38173c11f10b43f53cdfab09bae02601ebc9e288
SeveeN <maciej.hudek@gmail.com>
2025-12-21 15:14:05 +0100

    Sprawdzenie Pełnoletności

---
+++ ProgramyMatematyczne/Pelnoletnosc/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Pelnoletnosc;
+
+class Program
+{
+     static int Pelnoletnosc (int wiekUrz, int granica = 18)
+        {
+            if (wiekUrz >= granica)
+                return 1;
+            else
+                return 0;
+        }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Program sprawdzający pełnoletność użytkownika.");
+        Console.Write("Podaj swój wiek: ");
+        int wiekUrz = Convert.ToInt32(Console.ReadLine());
+        Pelnoletnosc(wiekUrz);
+        Console.WriteLine(Pelnoletnosc(wiekUrz) == 1 ? "Jesteś pełnoletni." : "Nie jesteś pełnoletni.");
+        
+
+    }
+}

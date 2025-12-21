dc37ba31f7afcc0cab2f9f64a77a72b33e83420a
SeveeN <maciej.hudek@gmail.com>
2025-12-21 15:30:00 +0100

    Celsius to Fahrenheit

---
+++ ProgramyMatematyczne/CelsiusFahrenheit/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CelsiusFahrenheit;
+
+class Program
+{
+    static double Convert(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Przelicznik temp. Celsjusza na Fahrenheita.");
+        Console.WriteLine("");
+        Console.WriteLine("Podaj temp. w st. Celsiusza:");
+        double celsius = double.Parse(Console.ReadLine());
+        double wynik = Convert(celsius);
+        Console.WriteLine("Temperatura w st. Fahrenheita wynosi: " + wynik + " F");
+    }
+}

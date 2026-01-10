d3894fccf3e7a8fadbd92644decac771d8e2b858
SeveeN <maciej.hudek@gmail.com>
2026-01-10 12:02:02 +0100

    SłownikProsty

---
+++ AplikacjeKonsolowe/SloWnikTlumaczen/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SloWnikTlumaczen;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Słownik tłumaczeń, gdzie klucz to słowo w języku angielskim, a wartość to tłumaczenie w języku polskim.");
+        Dictionary<string, string> translationDictionary = new Dictionary<string, string>
+        {
+            { "hello", "cześć" },
+            { "dog", "pjes" },
+            { "cat", "sierściuch" }
+        };
+        Console.WriteLine("Podaj słowo w języku angielskim do przetłumaczenia:");
+        string inputWord = Console.ReadLine();
+        if (translationDictionary.TryGetValue(inputWord, out string translatedWord))
+        {
+            Console.WriteLine($"Tłumaczenie słowa '{inputWord}' to '{translatedWord}'.");
+        }
+        else
+        {
+            Console.WriteLine($"Brak tłumaczenia dla słowa '{inputWord}' w słowniku.");
+        }
+    }
+}

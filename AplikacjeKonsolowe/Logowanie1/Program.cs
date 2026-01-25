3af828a35d5a34ecb7328a30ee5d188ffbee45be
mhSeveeN <maciej.hudek@gmail.com>
2026-01-25 16:25:23 +0100

    Aplikacja sprawdzająca logowanie

---
+++ AplikacjeKonsolowe/Logowanie1/Program.cs
@@ -0,0 +1,17 @@
+﻿string usrname = "user1234";
+string pass = "pass1234";
+
+
+Console.WriteLine("Witaj na portalu logowania!!");
+Console.WriteLine("Podaj nazwę użytkownika:");
+string username = Console.ReadLine();
+Console.WriteLine("Podaj hasło:");
+string password = Console.ReadLine();
+if (usrname == username && pass == password)
+{
+    Console.WriteLine("Zalogowano pomyślnie!");
+} 
+else 
+{
+    Console.WriteLine("Błędna nazwa użytkownika lub hasło.");
+}

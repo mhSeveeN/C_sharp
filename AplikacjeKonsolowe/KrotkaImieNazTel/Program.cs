14fce9e4f798fb880db9580e6fd3bab7e8575ed8
mhSeveeN <maciej.hudek@gmail.com>
2026-01-25 17:14:38 +0100

    Pierwsza krotka

---
+++ AplikacjeKonsolowe/KrotkaImieNazTel/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KrotkaImieNazTel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var person = (name: "Jan", surname: "Kowalski", phone: "123-456-789");
+        Console.WriteLine($"Imię i nazwisko: {person.name} {person.surname}, Telefon: {person.phone}");
+    }
+}
